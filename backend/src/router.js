fa47560d24431ae41f972128890991ce184118eb
Lucille-R <lucillerichard37@yahoo.fr>
2026-08-27 14:44:26 +0200

    Route code vendeur lr

--- backend/src/router.js
+++ backend/src/router.js
@@ -6,6 +6,24 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     console.log("La route est fonctionnelle");
-})
+});
+
+router.get("/vendeur", async (req, res) => {
+    const codeDonne = req.query.code;
+
+    if (codeDonne) {
+        const { rows } = await pool.query(`SELECT * FROM vendeurs WHERE numero_vendeur=$1`, [codeDonne]);
+
+        if (rows[0] == undefined) {
+            res.status(404).json({ "statut":"Vendeur inexistant" });
+        } else {
+            res.json(rows[0]);
+        }
+    }
+    
+    res.status(404).json({ "Statut": "Aucun code vendeur fourni" });
+
+});
+
 
 export default router;
