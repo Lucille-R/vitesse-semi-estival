4e81dbc5a7e694e9f33c0e70833f28b0eb20dbbc
Rémy L <remy.lagarre@gmail.com>
2026-08-27 15:09:45 +0200

    fix route code vendeur

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,22 +8,6 @@ router.get('/', async (req, res) => {
     console.log("La route est fonctionnelle");
 });
 
-router.get("/vendeur", async (req, res) => {
-    const codeDonne = req.query.code;
-
-    if (codeDonne) {
-        const { rows } = await pool.query(`SELECT * FROM vendeurs WHERE numero_vendeur=$1`, [codeDonne]);
-
-        if (rows[0] == undefined) {
-            res.status(404).json({ "statut":"Vendeur inexistant" });
-        } else {
-            res.json(rows[0]);
-        }
-    }
-    
-    res.status(404).json({ "Statut": "Aucun code vendeur fourni" });
-
-});
 
 
 router.get('/vendeur', async (req, res) => {
