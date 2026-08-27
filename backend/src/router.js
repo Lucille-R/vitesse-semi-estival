8fb0b4b9c97657da3efeb8bc83f563c909786e05
Rémy L <remy.lagarre@gmail.com>
2026-08-27 15:56:37 +0200

    add nouvelle vente

--- backend/src/router.js
+++ backend/src/router.js
@@ -33,4 +33,23 @@ router.get('/produits', async (req, res) => {
     res.json(rows);
 })
 
+router.post('/vente', async(req, res) => {
+    const maVente = req.body;
+
+    const now = new Date();
+    const dateJour = now.toISOString().slice(0, 10);
+    const heure = now.toTimeString().slice(0,8);
+
+    const { rows } = await pool.query(`INSERT INTO ventes (date, heure, vendeur_id) VALUES ($1, $2, $3) RETURNING *`, [dateJour, heure, maVente.vendeurId]);
+
+    const monID = rows[0].id;
+    const monPanier = maVente.lignes;
+
+    for (const a of monPanier) {
+        await pool.query(`INSERT INTO ventes_lignes (vente_id, produit_id, quantite) VALUES ($1, $2, $3)`, [monID, a.produitId, a.quantite]);
+    }
+
+    res.json({"vente":rows[0]});
+})
+
 export default router;
